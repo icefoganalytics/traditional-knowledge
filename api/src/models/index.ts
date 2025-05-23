dbdf2b405794684d50efdaf71e84b77d081b18f5
Marlen Brunner <klondikemarlen@gmail.com>
2025-05-23 11:20:58 -0700

    :sparkles: Add back-end modeling, controllers, policies, serializers, services, and routes for information sharing agreement archive items.
    AI gen source, with manual fixups, but I might have missed some stuff.

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -8,6 +8,7 @@ import Group from "@/models/group"
 import InformationSharingAgreement from "@/models/information-sharing-agreement"
 import InformationSharingAgreementAccessGrant from "@/models/information-sharing-agreement-access-grant"
 import InformationSharingAgreementAccessGrantSibling from "@/models/information-sharing-agreement-access-grant-sibling"
+import InformationSharingAgreementArchiveItem from "@/models/information-sharing-agreement-archive-item"
 import User from "@/models/user"
 import UserGroup from "@/models/user-group"
 
@@ -19,6 +20,7 @@ db.addModels([
   InformationSharingAgreement,
   InformationSharingAgreementAccessGrant,
   InformationSharingAgreementAccessGrantSibling,
+  InformationSharingAgreementArchiveItem,
   User,
   UserGroup,
 ])
@@ -30,6 +32,7 @@ ArchiveItemFile.establishScopes()
 Group.establishScopes()
 InformationSharingAgreement.establishScopes()
 InformationSharingAgreementAccessGrant.establishScopes()
+InformationSharingAgreementArchiveItem.establishScopes()
 User.establishScopes()
 UserGroup.establishScopes()
 
@@ -41,6 +44,7 @@ export {
   InformationSharingAgreement,
   InformationSharingAgreementAccessGrant,
   InformationSharingAgreementAccessGrantSibling,
+  InformationSharingAgreementArchiveItem,
   User,
   UserGroup,
 }
